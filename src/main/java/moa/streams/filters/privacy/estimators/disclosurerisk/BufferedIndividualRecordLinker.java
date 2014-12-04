d11c083cc91d50684bc44cf22475e4e59cb87597
David Martínez <gollumdeagol@gmail.com>
2014-12-04 17:34:17 +0100

    refactored distance to a metrics class and added IndividualRecordLinker as the primary (and dumb) disclosure risk estimator

--- src/main/java/moa/streams/filters/privacy/estimators/disclosurerisk/BufferedIndividualRecordLinker.java
+++ src/main/java/moa/streams/filters/privacy/estimators/disclosurerisk/BufferedIndividualRecordLinker.java
@@ -1,25 +1,87 @@
 package moa.streams.filters.privacy.estimators.disclosurerisk;
 
+import java.util.Vector;
+
 import moa.streams.filters.privacy.InstancePair;
+import moa.streams.filters.privacy.utils.Metrics;
+import weka.core.Instance;
 
 public class BufferedIndividualRecordLinker implements DisclosureRiskEstimator {
 
+	private Vector<Instance> originalInstancesBuffer;
+	
+	private final int bufferSize;
+		
+	private int recordLinkageHits;
+	
+	private int processedInstances;
+	
+	public BufferedIndividualRecordLinker(final int bufferSize) {
+		this.recordLinkageHits = 0;
+		this.processedInstances = 0;
+		this.bufferSize = bufferSize;
+		this.originalInstancesBuffer = new Vector<Instance>(bufferSize);
+	}
+	
+	public BufferedIndividualRecordLinker() {
+		this(100);
+	}
+	
+	public int getBufferSize() {
+		return bufferSize;
+	}
+	
 	@Override
 	public void restart() {
-		// TODO Auto-generated method stub
-		
+		this.recordLinkageHits = 0;
+		this.processedInstances = 0;
+		this.originalInstancesBuffer = new Vector<Instance>(bufferSize);
 	}
 	
 	@Override
 	public double getCurrentDisclosureRisk() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (double)recordLinkageHits / (double)processedInstances ;
 	}
 
 	@Override
 	public void estimateDisclosureRiskForInstancePair(InstancePair instancePair) {
-		// TODO Auto-generated method stub
-
+		//adds the instance, keeping the buffer with a maximum fixed size
+		addInstanceToBuffer(instancePair.originalInstance);
+		
+		//get nearest instances (indexes) to the anonymized one
+		int guess = guessNearestInstance(instancePair.originalInstance);
+		
+		//check if the nearest guesses contain the target one
+		if (guess == (originalInstancesBuffer.size() - 1)) {
+			++recordLinkageHits;
+		}
+	}
+	
+	private int guessNearestInstance(final Instance targetInstance) {
+		double minimum = 0.0;
+		int index = 0;
+		for (int i = 0; i < originalInstancesBuffer.size(); ++i) {
+			double distance = Metrics.distance(targetInstance, originalInstancesBuffer.get(i));
+			if (i == 0) {
+				minimum = distance;
+			}
+			else {
+				if (distance < minimum) {
+					minimum = distance;
+					index = i;
+				}
+			}
+		}
+		
+		return index;
+	}
+	
+	private void addInstanceToBuffer(Instance originalInstance) {
+		if (originalInstancesBuffer.size() >= bufferSize) {
+			originalInstancesBuffer.remove(0); //remove the last one
+		}
+		originalInstancesBuffer.add(originalInstance);
+		++processedInstances;
 	}
 
 }
---
+++ src/main/java/moa/streams/filters/privacy/utils/Metrics.java
@@ -0,0 +1,42 @@
+package moa.streams.filters.privacy.utils;
+
+import weka.core.Instance;
+
+public class Metrics {
+
+	/**
+	 * Computes the distance between two given instances, using the following scheme:
+	 * <br>
+	 * <pre>{@code
+	 *   procedure distance(x,y):
+	 *     dist := 0
+	 *     for each attribute i in x:
+	 *       if i != targetAttribute:
+	 *         if isNumeric(i):
+	 *           dist := dist + (x_i - y_i)^2
+	 *         else:
+	 *           if x_i != y_i:
+	 *             dist := dist + 1
+	 *     dist := sqrt(dist)
+	 *     return dist
+	 * }</pre>
+	 * 
+	 * @param x the first instance
+	 * @param y the second instance
+	 * @return the distance between the given instances
+	 */
+	public static double distance(Instance x, Instance y){
+		double dist = 0.0;
+		for (int i = 0; i < x.numAttributes(); ++i){
+			if (i != x.classIndex()) { //skip all those variables that are the target class
+				if (x.attribute(i).isNumeric()){
+					dist += (x.value(i) - y.value(i)) * (x.value(i) - y.value(i));
+				} else {
+					dist += (x.value(i) !=  y.value(i)) ? 1.0 : 0.0;
+				}
+			}
+		}
+		return Math.sqrt(dist);
+	}
+	
+}
